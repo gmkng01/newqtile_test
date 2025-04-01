7f350193db288137f022f20c7649d1f51ed27f55
Abhishek Mishra <gmkng1@gmail.com>
2025-04-01 17:06:28 +0530

    changes in nvim

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pylsp", "gopls", "bashls" }
+				ensure_installed = { "lua_ls", "pylsp", "gopls" }
 			})
 		end
 	},
@@ -26,9 +26,9 @@ return {
 			lspconfig.gopls.setup({
 				capabilities = capabilities
 			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities
-			})
+			-- lspconfig.bashls.setup({
+			-- 	capabilities = capabilities
+			-- })
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
